95306ae21f1c91c6b1cc8cb28848b183d6e5ebdb
Ferri Kusuma <cklothox79@gmail.com>
2025-11-06 06:46:58 +0700

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def utc_to_wib(utc_time_str):
     try:
         dt = datetime.fromisoformat(utc_time_str)
-        wib = dt.hour + 7
-        return f"{wib%24:02d}:{dt.minute:02d}"
-    except:
+        wib_hour = (dt.hour + 7) % 24
+        return f"{wib_hour:02d}:{dt.minute:02d}"
+    except Exception:
         return utc_time_str
